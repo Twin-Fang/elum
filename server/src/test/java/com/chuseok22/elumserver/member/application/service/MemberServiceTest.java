cca5e252acbb33efaeb5e514b5f37fb345f79cd6
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2026-09-19 01:48:45 +0900

    보호자 계정과 당사자 프로필 분리 및 라이선스 구조 : test : 탈퇴 테스트에 구독 저장소를 넣는다. 탈퇴 로직에 구독 정리를 더하면서 테스트에 대역을 추가하지 않아 네 건이 깨져 있었다 https://github.com/Twin-Fang/elum/issues/158

--- server/src/test/java/com/chuseok22/elumserver/member/application/service/MemberServiceTest.java
+++ server/src/test/java/com/chuseok22/elumserver/member/application/service/MemberServiceTest.java
@@ -14,6 +14,7 @@ import com.chuseok22.elumserver.auth.infrastructure.repository.RefreshTokenRepos
 import com.chuseok22.elumserver.common.infrastructure.exception.CustomException;
 import com.chuseok22.elumserver.common.infrastructure.exception.ErrorCode;
 import com.chuseok22.elumserver.license.application.service.EntitlementService;
+import com.chuseok22.elumserver.license.infrastructure.repository.SubscriptionRepository;
 import com.chuseok22.elumserver.member.application.dto.request.MemberCharacterUpdateRequest;
 import com.chuseok22.elumserver.member.application.dto.response.MemberResponse;
 import com.chuseok22.elumserver.member.infrastructure.entity.CharacterType;
@@ -56,6 +57,9 @@ class MemberServiceTest {
 
   @Mock
   private DeviceLinkRepository deviceLinkRepository;
+
+  @Mock
+  private SubscriptionRepository subscriptionRepository;
 
   @Mock
   private EntitlementService entitlementService;
